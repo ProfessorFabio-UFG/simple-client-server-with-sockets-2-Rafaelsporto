bc4a41dcd6826bc63f8f697af9048cbbc72be935
Rafaelsporto <94205883+Rafaelsporto@users.noreply.github.com>
2025-03-30 22:11:14 -0300

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,9 @@ from constCS import * #-
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
+entradas = ["add 3 2" , "subtract 3 2", "multiply 3 2", "divide 3 2"]
+for input in entradas:
+  s.send(str.encode(input))
+  result = bytes.decode(s.recv(1024))
+  print (input+" results in "+str(result))# print the result
 s.close()               # close the connection
